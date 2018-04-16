f9d4e836ff21b669a98c89de98e735a949acfda5
Camilo Guio Suarez <camiloguio@gmail.com>
2018-04-16 15:34:20 -0500

    galeria rigidos

--- loop.js
+++
@@ -1,8 +0,0 @@
-var onepart = "<div class='grid__item' data-size='1280x961'> <a href='galeria_rigidos/";
-var secondpart = ".jpg class='img-wrap'><img src='galeria_rigidos/";
-var thirdpart = ".jpg' alt='img";
-var fourpart = "' /> <div class='description description--grid'>Digitos y diseños</div></a></div>";
-
-for(i=32; i<=71; i++){
-    console.log(onepart + i + secondpart + i+ thirdpart + i + fourpart);
-    }
